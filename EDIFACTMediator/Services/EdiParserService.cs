52b6dbb0bef8df67c9ac91cb5f7e2db06ea07686
ArnoSchiller <arno-schiller@hotmail.de>
2024-10-28 11:18:17 +0100

    Added one job per edi task

---
+++ EDIFACTMediator/Services/EdiParserService.cs
@@ -0,0 +1,22 @@
+﻿using indice.Edi;
+
+namespace EDIFACTMediator.Services;
+
+public static class EdiParserService
+{
+    public static T? FromEdi<T>(string ediString)
+    {
+        try
+        {
+            var grammar = EdiGrammar.NewEdiFact();
+
+            using var reader = new StringReader(ediString);
+            var ediOrders = new EdiSerializer().Deserialize<T>(reader, grammar);
+            return ediOrders;
+        }
+        catch (Exception e)
+        {
+            return default(T);
+        }
+    }
+}
