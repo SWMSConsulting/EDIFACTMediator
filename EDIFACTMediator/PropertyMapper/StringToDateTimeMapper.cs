5d3304a031740eef8813ef4032b4c0d888f9a196
ArnoSchiller <arno-schiller@hotmail.de>
2025-01-27 10:46:20 +0100

    added new mapper

---
+++ EDIFACTMediator/PropertyMapper/StringToDateTimeMapper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace EDIFACTMediator.PropertyMapper;
+
+public class StringToDateTimeMapper : IPropertyMapper
+{
+    public IEnumerable<string> RequiredParameters => [];
+
+    public Dictionary<string, string> DefaultValues => new Dictionary<string, string>();
+
+    public object? Map(object? source, Dictionary<string, string> parameters, object? sourceBase)
+    {
+        var sourceString = source as string;
+
+        if(string.IsNullOrEmpty(sourceString))
+            return null;
+
+        DateTime.TryParse(sourceString, out DateTime result);
+
+        return result;
+    }
+}
