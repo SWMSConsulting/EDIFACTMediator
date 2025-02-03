0ff241dcde63654db1ddc4a943ff82e4da6ad57a
ArnoSchiller <arno-schiller@hotmail.de>
2025-02-03 13:31:44 +0100

    added string format mapper

---
+++ EDIFACTMediator/PropertyMapper/StringFormatMapper.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EDIFACTMediator.PropertyMapper;
+
+public class StringFormatMapper : IPropertyMapper
+{
+    public IEnumerable<string> RequiredParameters => DefaultValues.Keys;
+
+    public Dictionary<string, string> DefaultValues => new Dictionary<string, string>
+    {
+        ["Format"] = "{0}"
+    };
+
+    public object? Map(object? source, Dictionary<string, string> parameters, object? sourceBase)
+    {
+        var format = parameters.GetValueOrDefault("Format");
+        if (string.IsNullOrEmpty(format) || !format.Contains("{0}"))
+        {
+            return null;
+        }
+        return string.Format(format, source);
+    }
+}
